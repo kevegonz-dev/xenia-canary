34ed795b111216a1387d9b40e47d23c2f946c1fe
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2026-04-11 20:29:42 +0100

    [XSocket] Changed X_WSA_OPERATION_ABORTED to X_WSAECANCELLED

--- src/xenia/kernel/xsocket.cc
+++ src/xenia/kernel/xsocket.cc
@@ -438,7 +438,8 @@ int XSocket::WSAPollWrite(bool wait, X_WSA_ERROR* error) {
 
     if (cancel_overlapped_) {
       if (error) {
-        *error = X_WSA_ERROR::X_WSA_OPERATION_ABORTED;
+        *error = X_WSA_ERROR::X_WSAECANCELLED;
+        // *error = X_HRESULT_FROM_WIN32( X_WSA_ERROR::X_WSAECANCELLED);
         activity = X_SOCKET_ERROR;
       }
     }
@@ -640,9 +641,9 @@ int XSocket::WSASendTo(XWSABUF* buffers, uint32_t num_buffers,
     XELOGI("{}:: failed with error code {}", __func__,
            static_cast<uint32_t>(wsa_error));
 
-    if (wsa_error == X_WSA_ERROR::X_WSA_OPERATION_ABORTED) {
-      XELOGD("{}:: Operation Aborted!", __func__);
-      XWSASetLastError(X_WSA_ERROR::X_WSA_OPERATION_ABORTED);
+    if (wsa_error == X_WSA_ERROR::X_WSAECANCELLED) {
+      XELOGD("{}:: Operation Cancelled!", __func__);
+      XWSASetLastError(X_WSA_ERROR::X_WSAECANCELLED);
     }
   }
 
@@ -662,7 +663,8 @@ int XSocket::WSAPollRead(bool wait, X_WSA_ERROR* error) {
 
     if (cancel_overlapped_) {
       if (error) {
-        *error = X_WSA_ERROR::X_WSA_OPERATION_ABORTED;  // X_WSAECANCELLED?
+        *error = X_WSA_ERROR::X_WSAECANCELLED;
+        // *error = X_HRESULT_FROM_WIN32( X_WSA_ERROR::X_WSAECANCELLED);
         activity = X_SOCKET_ERROR;
       }
     }
@@ -870,9 +872,9 @@ int XSocket::WSARecvFrom(XWSABUF* buffers, uint32_t num_buffers,
     XELOGI("{}:: failed with error code {}", __func__,
            static_cast<uint32_t>(wsa_error));
 
-    if (wsa_error == X_WSA_ERROR::X_WSA_OPERATION_ABORTED) {
-      XELOGD("{}:: Operation Aborted!", __func__);
-      XWSASetLastError(X_WSA_ERROR::X_WSA_OPERATION_ABORTED);
+    if (wsa_error == X_WSA_ERROR::X_WSAECANCELLED) {
+      XELOGD("{}:: Operation Cancelled!", __func__);
+      XWSASetLastError(X_WSA_ERROR::X_WSAECANCELLED);
     }
   }
 
@@ -976,8 +978,8 @@ bool XSocket::WSAGetOverlappedResult(XWSAOVERLAPPED* overlapped_ptr,
 
       return true;
     } break;
-    case X_WSA_ERROR::X_WSA_OPERATION_ABORTED: {
-      XELOGD("{}:: Operation Aborted!", __func__);
+    case X_WSA_ERROR::X_WSAECANCELLED: {
+      XELOGD("{}:: Operation Cancelled!", __func__);
       XWSASetLastError(internal_result);
     } break;
     case X_WSA_ERROR(X_STATUS_PENDING):
